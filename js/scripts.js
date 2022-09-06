cfc29ca4af25ca5cc28b0715566a71e519a3eb45
alessandro camargo <acamargodias@terra.com.br>
2022-09-06 06:53:59 -0300

    footer e cards

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,21 @@ skills.forEach((elemento, index) => {
 const year = document.getElementById("year");
 const yearCurrent = new Date();
 year.innerHTML = yearCurrent.getFullYear();
+
+function upButton() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+function showHideButton() {
+  if (window.scrollY === 0) {
+    document.querySelector(".upbutton").style.display = "none";
+  } else {
+    document.querySelector(".upbutton").style.display = "block";
+  }
+}
+
+window.addEventListener("scroll", showHideButton);
